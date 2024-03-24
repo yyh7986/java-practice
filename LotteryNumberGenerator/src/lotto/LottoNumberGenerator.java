cc91b1004d1beb3ffe38c1930e6d65e6e9bbf674
yyh7986 <yyh7986@gmail.com>
2024-03-24 18:20:36 +0900

    add bubble sort part

--- LotteryNumberGenerator/src/lotto/LottoNumberGenerator.java
+++ LotteryNumberGenerator/src/lotto/LottoNumberGenerator.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class LottoNumberGenerator {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 1; i++) {
             LottoNumberGenerator.lottoNumberGenerate();
         }
     }
@@ -26,15 +26,30 @@ public class LottoNumberGenerator {
         // 섞인 공들 중 앞에서 6개 뽑기
         int[] lottoNumbers = new int[6];
         System.arraycopy(ball, 0, lottoNumbers, 0, lottoNumbers.length);
-//        System.out.println(Arrays.toString(lottoNumbers));
+        System.out.println(Arrays.toString(lottoNumbers));
 
         // 오름차순 정렬
-        for (int i = 1; i < lottoNumbers.length; i++) {
+        /*for (int i = 1; i < lottoNumbers.length; i++) {
             for (int j = 0; j < i; j++) {
                 if(lottoNumbers[j] > lottoNumbers[i]){
                     int temp = lottoNumbers[j];
                     lottoNumbers[j] = lottoNumbers[i];
                     lottoNumbers[i] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(lottoNumbers));*/
+
+        // 버블정렬
+        for (int i = 0; i < lottoNumbers.length - 1; i++) {
+            boolean changed = false;
+
+            for (int j = 0; j < lottoNumbers.length - 1 - i; j++) {
+                if(lottoNumbers[j] > lottoNumbers[j + 1]){
+                    int temp = lottoNumbers[j];
+                    lottoNumbers[j] = lottoNumbers[j + 1];
+                    lottoNumbers[j + 1] = temp;
+                    changed = true;
                 }
             }
         }
